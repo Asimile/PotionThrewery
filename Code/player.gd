86fb29a236c26e1056552e3865aee6f4d5b48d6c
Asimile <AsimNRaja@gmail.com>
2024-07-22 22:16:43 +0200

    Initial commit
    Create a player scene and allows them to move.

---
+++ Code/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var INPUT_VECTOR = Vector2(0, 0)
+
+@export var move_speed: float = 200.0
+
+func _physics_process(delta):
+	#get input direction
+	
+	INPUT_VECTOR.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	INPUT_VECTOR.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	
+	velocity = INPUT_VECTOR * move_speed
+	
+	print(INPUT_VECTOR)
+	
+	move_and_slide()
