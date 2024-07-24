355bf25dbdffac28628bf3ad7045a0a3946744d9
Asimile <AsimNRaja@gmail.com>
2024-07-25 00:01:09 +0200

    Implement potion throwing
    Player can now throw a potion in the direction that they aim. Collisions are still weird, but potions going the right direction is a big win.
    
    I'm worried about how it seems the player can't throw potions while moving.
    
    Next up is either potion impact collision or enemies.

--- Code/player.gd
+++ Code/player.gd
@@ -2,7 +2,12 @@ extends CharacterBody2D
 
 var INPUT_VECTOR = Vector2(0, 0)
 
-@export var move_speed: float = 200.0
+var move_speed: float = 150.0
+
+@export var Potion :PackedScene
+
+@onready var PLAYER_SPRITE = $PlayerSprite
+@onready var THROW_POSITION = $PlayerSprite/ThrowPosition
 
 func _ready():
 	pass
@@ -18,4 +23,17 @@ func _physics_process(delta):
 	#print(INPUT_VECTOR)
 	
 	move_and_slide()
-	look_at(get_global_mouse_position())
+	PLAYER_SPRITE.look_at(get_global_mouse_position())
+	
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("throw"):
+		throw_potion()
+
+func throw_potion():
+	var potion_instance = Potion.instantiate()
+	add_child(potion_instance)
+	potion_instance.global_position = THROW_POSITION.global_position
+	var target = get_global_mouse_position()
+	var direction_to_mouse = potion_instance.global_position.direction_to(target).normalized()
+	potion_instance.set_direction(direction_to_mouse)
--- Scenes/potion.gd
+++ Scenes/potion.gd
@@ -1,9 +1,23 @@
 extends Node2D
 
+var potion_speed: float = 5.0
+
+var direction: Vector2 = Vector2.ZERO
+
 @onready var POTION_SPRITE = $PotionSprite
 
 func _ready():
 	pass
+
+func _physics_process(delta: float):
+	if direction != Vector2.ZERO:
+		var velocity = direction * potion_speed
+		
+		global_position += velocity
+		rotate(deg_to_rad(15.0))
+
+func set_direction(direction: Vector2):
+	self.direction = direction
 
 func _on_color_picker_button_color_changed(color):
 	POTION_SPRITE.material.set_shader_parameter("NEW_COLOR", color)
